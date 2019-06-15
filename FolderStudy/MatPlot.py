4be06e7724e1cff1bbe9eff1f27991059b4723ef
xin.pu <pu.xin@outlook.com>
2019-06-15 09:35:22 +0100

    create Venv

--- FolderStudy/MatPlot.py
+++ FolderStudy/MatPlot.py
@@ -1,10 +1,14 @@
-import numpy as np 
-from matplotlib import pyplot as plt 
- 
-x = np.arange(1,11) 
-y =  2  * x +  5 
-plt.title("Matplotlib demo") 
-plt.xlabel("x axis caption") 
-plt.ylabel("y axis caption") 
-plt.plot(x,y) 
-plt.show()
+
+
+
+import request
+
+
+x = range(1,11) 
+print(x[0])
+# y =  2  * x +  5 
+# plt.title("Matplotlib demo") 
+# plt.xlabel("x axis caption") 
+# plt.ylabel("y axis caption") 
+# plt.plot(x,y) 
+# plt.show()pyto 
